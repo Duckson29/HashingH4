92a735bac41219e9bcee93d70e8ffcc8d2a06635
zbcanols21 <zbcanols21@zbc.dk>
2020-10-27 10:33:08 +0100

    Add project files.

---
+++ Hashing/HMAC.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleApp1.Hashing
+{
+    class MyHMAC
+    {
+        private HMAC hashingAlgo;
+        //public void HashBytes(byte[] passToHas)
+        //{
+
+        //}
+        public byte[] HashBytes(byte[] passToHas, byte[] hashKey)
+        {
+            hashingAlgo.Key = hashKey;
+            byte[] value = hashingAlgo.ComputeHash(passToHas);
+            return value;
+        }
+        public bool CheckAuth(byte[] msg, byte[] mac, byte[] key)
+        {
+            hashingAlgo.Key = key;
+            if (ByteLoob(hashingAlgo.ComputeHash(msg), mac, (hashingAlgo.HashSize / 8)))
+                return false;
+            else
+                return true;
+        }
+        public bool ByteLoob(byte[] hashedMsg, byte[] revicemsg, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (revicemsg[i] != hashedMsg[i])
+                    return false;
+            }
+            return true;
+        }
+        public void HashToUse(int hashToUse)
+        {
+            switch (hashToUse)
+            {
+                case 1:
+                    hashingAlgo = new HMACMD5();
+                    break;
+                case 2:
+                    hashingAlgo = new HMACSHA1();
+                    break;
+                case 3:
+                    hashingAlgo = new HMACSHA256();
+                    break;
+                case 4:
+                    hashingAlgo = new HMACSHA512();
+                    break;
+                default:
+                    hashingAlgo = new HMACMD5();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp1.Hashing;
+using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHMAC mAC = new MyHMAC();
+
+            Console.WriteLine("1: md5\n2: sha1\n3: sha256\n4: sha512");
+
+            int data = 0;
+            int.TryParse(Console.ReadLine(),out data);
+            mAC.HashToUse(data);
+
+            Console.WriteLine("Hashing of pwd");
+            Console.WriteLine("Input keyString to use in hmac");
+            byte[] keyString = Encoding.UTF8.GetBytes(Console.ReadLine());            
+            Console.WriteLine("Input password.");
+            byte[] passwordByte = Encoding.UTF8.GetBytes(Console.ReadLine());
+            byte[] output = mAC.HashBytes(passwordByte, keyString);
+            Console.WriteLine($"ASCII: {Convert.ToBase64String(output)}\n HEX: {Encoding.UTF8.GetChars(output)[2]}");
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
